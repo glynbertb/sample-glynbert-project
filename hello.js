e25c41a8383368941b8d89c67368eb812559af47
Jay-R Joseph Gabunada <jayr@23point5.com>
2025-07-19 16:53:15 +0800

    added new console log

--- hello.js
+++ hello.js
@@ -1,3 +1,8 @@
 console.log("Hello, World!");
 console.log('This is a test message.');
 console.log(`Current date and time: ${new Date().toLocaleString()}`);
+console.log('Goodbye!');
+console.log("End of script.");
+console.log("This script demonstrates basic logging in JavaScript.");
+console.log('You can use single quotes, double quotes, or backticks for strings.');
+console.log("Remember to check your console for output.");
