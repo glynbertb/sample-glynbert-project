9d1488a9d5a82115726ba465d485eb72f539e498
Glynbert Baquiran <glynbertb@gmail.com>
2025-07-19 16:59:09 +0800

    design

--- hello.js
+++ hello.js
@@ -6,3 +6,7 @@ console.log("End of script.");
 console.log("This script demonstrates basic logging in JavaScript.");
 console.log('You can use single quotes, double quotes, or backticks for strings.');
 console.log("Remember to check your console for output.");
+console.log("This is a test message.");
+console.log(`Current date and time: ${new Date().toLocaleString()}`);       
+console.log('Goodbye!');
+console.log("End of script.");  
